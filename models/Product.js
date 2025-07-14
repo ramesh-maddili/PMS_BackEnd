2fe247a4af7daa9890d172d6e50dcd89c2ab9f24
ramesh-maddili <ramesh.maddili@fissionlabs.com>
2025-07-14 12:00:37 +0530

    product_property

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  category: String
+  name: {
+    type: String,
+    required: true,     //  must be inside an object
+    trim: true,
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: true,     //  must be inside an object
+    min: 0
+  },
+  category: {
+    type: String,
+    required: true,     //  must be inside an object
+    enum: ['Electronics', 'Clothing', 'Laptop', 'Accessories', 'Footwear']
+  },
+  inStock: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Product', productSchema);
